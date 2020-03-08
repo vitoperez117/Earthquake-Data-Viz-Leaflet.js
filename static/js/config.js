a0465057f90b5acb0068828f5d4c1d650b90f496
Vito <vito@Vitos-MacBook-Pro.local>
2020-03-08 16:58:24 -0700

    my first commit

---
+++ static/js/config.js
@@ -0,0 +1,1 @@
+var API_KEY = "pk.eyJ1Ijoidml0b3BlcmV6MTE3IiwiYSI6ImNrNW9wb2FjaDBxNnAzanFoZWVnMGphdjEifQ.QQj93p_z5umsEhPYHbWP3Q"
---
+++ static/js/logic.js
@@ -0,0 +1,1 @@
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then(a => console.log(a))
